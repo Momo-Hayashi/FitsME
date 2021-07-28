648508af81d283bcb0e79175dfc665df674ec9e7
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-07-28 17:41:16 +0900

    #18 Create: Favorite model and associated it with users and clothes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   validates :username, presence: true, length: { maximum: 60 }
   validates :points, presence: true
+
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_clothes, through: :favorites, source: :clothe
 end
