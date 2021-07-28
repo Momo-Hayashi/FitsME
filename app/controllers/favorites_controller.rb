3134861164773df43d056240fa40f0f2d21ca8c1
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-07-28 18:41:41 +0900

    #18 Add: favorite button on clothes page

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,16 @@
 class FavoritesController < ApplicationController
 
-  def show
-    @favorite = current_user.favorites.find_by(clothe_id: @clothe.id)
+  def create
+    favorite = current_user.favorites.create(clothe_id: params[:clothe_id])
+    redirect_to clothe_path(params[:clothe_id]), notice: "#{favorite.clothe.name}をお気に入り登録しました！"
   end
+
+  def show
+  end
+
+  def destroy
+    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    redirect_to clothes_path, notice: "#{favorite.clothe.name}をお気に入り解除しました！"
+  end
+
 end
