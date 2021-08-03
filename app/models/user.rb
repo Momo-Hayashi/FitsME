613b17c16d698f12a2320d72a0fff0e1ebf30fdd
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-08-03 12:09:03 +0900

    #24 Create: created user for guest-login using user model and users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :username, presence: true, length: { maximum: 60 }
+
+  validates :username, presence: true, length: { in: 1..30 }
   validates :points, presence: true
 
   has_many :favorites, dependent: :destroy
   has_many :favorite_clothes, through: :favorites, source: :clothe
   has_many :orders
   has_many :carts, dependent: :destroy
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.username = 'ゲスト'
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
+
 end
