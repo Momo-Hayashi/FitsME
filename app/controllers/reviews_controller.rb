4e0962c0eb081cdf187f04ebb9c5838cd63c7b8b
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-08-06 14:57:57 +0900

    #26 Create: created reviews contrller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,27 @@
+class ReviewsController < ApplicationController
+
+  def new
+
+  end
+
+  def confirm
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+end
