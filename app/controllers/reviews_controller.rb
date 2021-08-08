09fa24318e46bd26ec603b43169c652b3b27592a
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-08-08 12:25:52 +0900

    #39 Add: added edit and destroy actions to reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :set_clothe, only: %i[ new confirm create edit update]
+  before_action :set_reviews, only:  %i[ show edit destroy update ]
 
   def new
     @stock = Stock.find(params[:stock_no])
@@ -26,22 +27,31 @@ class ReviewsController < ApplicationController
     end
   end
 
-  def show
-
+  def edit
+    @stock = Stock.find(params[:stock_no])
   end
 
   def update
-
+    if @review.update(review_params)
+      redirect_to clothe_path(@review.clothe), notice: "Successfully updated!"
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @review.destroy
+    redirect_to clothe_path(@review.clothe), notice: "Successfully deleted!"
   end
 
   private
 
   def review_params
     params.require(:review).permit( :clothe_id, :user_id, :rate, :content, :stock_no, images: [])
+  end
+
+  def set_reviews
+    @review = Review.find(params[:id])
   end
 
   def set_clothe
