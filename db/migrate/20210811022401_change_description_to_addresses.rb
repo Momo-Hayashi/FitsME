a7b8e95999eb42b17785591be9d894aff3261bcf
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-08-11 11:37:49 +0900

    #54 Fix: removed null:false from addresses table

---
+++ db/migrate/20210811022401_change_description_to_addresses.rb
@@ -0,0 +1,11 @@
+class ChangeDescriptionToAddresses < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null :addresses, :last_name, true
+    change_column_null :addresses, :first_name, true
+    change_column_null :addresses, :postcode, true
+    change_column_null :addresses, :prefecture_code, true
+    change_column_null :addresses, :address_city, true
+    change_column_null :addresses, :address_street, true
+    change_column_null :addresses, :phone_number, true
+  end
+end
