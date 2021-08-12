046d436c9dfbc5bfabcf00fe33c27e7161ac402b
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-08-12 11:26:49 +0900

    Add: added placeholders to all the forms in views

--- app/controllers/retailers/registrations_controller.rb
+++ app/controllers/retailers/registrations_controller.rb
@@ -49,12 +49,12 @@ class Retailers::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone_number, :address, :website_url, :logo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :description, :logo, :website_url, :email, :phone_number, :address])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone_number, :address, :website_url, :logo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :description, :logo, :website_url, :email, :phone_number, :address])
   end
 
   # The path used after sign up.
