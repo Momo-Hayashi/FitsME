d1bfbb0234ef0db95ec355bd8777659ca21aad3c
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-08-12 12:57:06 +0900

    Add: added autheticate access controll to all controllers

--- app/controllers/retailers/registrations_controller.rb
+++ app/controllers/retailers/registrations_controller.rb
@@ -1,13 +1,22 @@
 # frozen_string_literal: true
 
 class Retailers::RegistrationsController < Devise::RegistrationsController
+  before_action :authenticate_retailer!
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
   before_action :ensure_normal_retailer, only: %i[ update destroy ]
+  before_action :ensure_not_user
+
 
   def ensure_normal_retailer
     if resource.email == 'guest_retailer@example.com'
       redirect_to root_path, notice: 'ゲストリテイラーの更新・削除はできません。'
+    end
+  end
+
+  def ensure_not_user
+    if current_user.present?
+      redirect_to root_path, notice: 'アクセス権限がありません'
     end
   end
 
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,10 +4,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
   before_action :ensure_normal_user, only: %i[ update destroy ]
+  before_action :authenticate_user!
+  before_action :ensure_not_retailer
 
   def ensure_normal_user
     if resource.email == 'guest@example.com'
       redirect_to root_path, notice: 'ゲストユーザーの更新・削除はできません。'
+    end
+  end
+
+  def ensure_not_retailer
+    if current_retailer.present?
+      redirect_to root_path, notice: 'アクセス権限がありません'
     end
   end
 
