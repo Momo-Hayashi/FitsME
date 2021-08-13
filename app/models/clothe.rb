0a001be63f2e1dcb79e20b43996bae23348e2385
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-08-13 15:42:39 +0900

    #65 Add: validation for child model(stocks table)

--- app/models/clothe.rb
+++ app/models/clothe.rb
@@ -4,9 +4,14 @@ class Clothe < ApplicationRecord
   has_many :categorizations
   has_many :categories, through: :categorizations, source: :category
   has_many :favorites, dependent: :destroy
-  has_many :stocks, dependent: :destroy
   has_many :reviews, dependent: :destroy
+
+  has_many :stocks, dependent: :destroy
   accepts_nested_attributes_for :stocks, allow_destroy: true, reject_if: :all_blank
+  validates :stocks, presence: true
+  # validates :size, presence: true
+  # validates :color, presence: true
+  # validates :quantity, presence: true
 
   validates :name, presence: true
   validates :price, presence: true
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,9 @@
 class Stock < ApplicationRecord
-  belongs_to :clothe
+  belongs_to :clothe, optional: true
+  validates :size, presence: true
+  validates :color, presence: true
+  validates :quantity, presence: true
+  
   has_many :carts
   has_many :order_stocks
   has_many :bought_orders, through: :order_stocks, source: :order
