59467b06c7ac4ac3ef0e2cadb22d17c94de5b83d
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-08-13 16:28:18 +0900

    #66 Fix: changed the size and color columns in stocks to integer

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,4 +3,9 @@ class Stock < ApplicationRecord
   has_many :carts
   has_many :order_stocks
   has_many :bought_orders, through: :order_stocks, source: :order
+
+  enum color: { White:0, Black:1, Blue:2, Brown:3, Green:4, Purple:5, Yellow:6,
+              Pink:7, Red:8, Orange:9, Silver:10, Gold:11, Floral:12, Grey:13, Beige:14, 'Mixed Colors':15, Other:16 }
+  enum size: { 'one-size':0, '~XS':1, S:2, M:3, L:4, 'XL~':5 }
+  
 end
---
+++ db/migrate/20210813070448_change_colomns_to_stocks.rb
@@ -0,0 +1,7 @@
+class ChangeColomnsToStocks < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :stocks, :color
+    remove_column :stocks, :size
+    
+  end
+end
