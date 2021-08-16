dbadb948aa7069cd803fb7b3aabf5837401e3e9e
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-08-16 17:38:51 +0900

    #71 Add: set Tokyo as time-zone

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module FitsME
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
