e00f3506671a87cd3834d57b28137dc73d0879c1
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-08-16 14:35:27 +0900

    #69 Add: added categories to search form

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,19 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
+    #対象のカテゴリーの服のidを配列でとりだす
     @categorizations = Categorization.where(category_id: @category.id).pluck(:clothe_id)
-    @clothes = Clothe.find(@categorizations)
+
+    @q = Clothe.ransack(params[:q])
+    @results = @q.result(distinct: true)
+
+    @clothes_ids = []
+    @results.each do |result|
+      if @categorizations.include?(result.id)
+        @clothes_ids.push(result.id)
+      end
+    end
+    @clothes = Clothe.find(@clothes_ids)
   end
+
 end
