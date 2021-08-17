d5162f3d21bc04ff97da8f7437e142094b39a051
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-08-17 14:26:52 +0900

    #79 Fix: fixed view and jQuery

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_13_072338) do
+ActiveRecord::Schema.define(version: 2021_08_17_045612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,7 +62,6 @@ ActiveRecord::Schema.define(version: 2021_08_13_072338) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "current_address"
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
