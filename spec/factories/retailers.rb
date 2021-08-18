a8bc9490762c8fc1097b8d4e48a92222490f5fb4
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-08-18 14:34:43 +0900

    #81 Add: added system-spec test for Retailer and Admin

--- spec/factories/retailers.rb
+++ spec/factories/retailers.rb
@@ -8,4 +8,13 @@ FactoryBot.define do
     password { 'test_retailer1@test.com' }
   end
 
+  factory :second_retailer, class:Retailer do
+    name { '削除リテイラー' }
+    address {'東京都渋谷区渋谷市1-2-3'}
+    email { 'test_retailer1@test.com' }
+    website_url { 'https://diveintocode.jp/' }
+    phone_number { '1234567890' }
+    password { 'test_retailer1@test.com' }
+  end
+
 end
