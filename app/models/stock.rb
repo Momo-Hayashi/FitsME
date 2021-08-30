a8fccee8c38cdbefc78c0288ce54d7987d49dee5
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-08-31 08:35:19 +0900

    #128 Fix: fixed the bug retailers could not delete clothes which were already bought

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,8 +5,8 @@ class Stock < ApplicationRecord
   validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   has_many :carts
-  has_many :order_stocks
-  has_many :bought_orders, through: :order_stocks, source: :order
+  has_many :order_stocks, dependent: :destroy
+  has_many :bought_orders, through: :order_stocks, source: :order, dependent: :destroy
 
   enum color: { White:0, Black:1, Blue:2, Brown:3, Green:4, Purple:5, Yellow:6,
               Pink:7, Red:8, Orange:9, Silver:10, Gold:11, Floral:12, Grey:13, Beige:14, 'Mixed Colors':15, Other:16 }
