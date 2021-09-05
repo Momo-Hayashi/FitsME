f5ccc286b7b618e92d426248fd27557cc286986e
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-09-05 12:24:28 +0900

    #131 Fix: fixed the system-spec-tests for order-function

--- spec/system/order_spec.rb
+++ spec/system/order_spec.rb
@@ -3,25 +3,23 @@ RSpec.describe '購入機能', type: :system do
 
   let!(:retailer) { FactoryBot.create(:retailer) }
   let!(:clothe) { FactoryBot.create(:clothe, retailer: retailer) }
-  let!(:address) { FactoryBot.create(:address) }
-  let!(:user) { FactoryBot.create(:user, email:'test@test.jp') }
-
+  let!(:user) { FactoryBot.create(:user) }
 
   describe '服の購入機能' do
 
     def user_login
       visit new_user_session_path
-      fill_in 'user_email', with: 'test@test.jp'
+      fill_in 'user_email', with: 'testuser1@test.com'
       fill_in 'user_password', with: 'testuser1@test.com'
-      find(:xpath, '//*[@id="new_user"]/div[4]/input').click
+      find(:xpath, '//*[@id="new_user"]/div[2]/input').click
       sleep(0.1)
-      click_on 'Fits ME'
+      find(:xpath, '/html/body/header/ul/li/a/img').click
+      sleep(0.1)
     end
 
     context 'ユーザーが服を購入した場合' do
       it '購入履歴に表示される', js: true do
         user_login
-        sleep(0.1)
         click_on 'センタープレスセミフレアスラックス'
         sleep(0.1)
         click_on 'Add to Cart'
